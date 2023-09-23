6cb26f4484cf3e0a68e33e02da1466adc2d5a180
Callum Pease <cpease1997@hotmail.com>
2023-09-23 18:27:15 +0100

    Update opening message

--- services/ticket_open.rb
+++ services/ticket_open.rb
@@ -6,7 +6,7 @@ module AutoArchive
       if event.channel.name.downcase.start_with?(ENV["TICKET_CHANNEL_PREFIX"].downcase)
         # Start listening for messages in the newly created channel
         if ENV["NOTIFY_ARCHIVE_START"]&.downcase == "true"
-          event.channel.send_message("Please upload all relevant files to Discord, YouTube, Streamable, MedalTV or Gyazo.")
+          event.channel.send_message("Please upload all relevant files to Discord, YouTube, Twitch Clips, Streamable, MedalTV or Gyazo.")
         end
       end
     end
